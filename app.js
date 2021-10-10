526bc4fbd01d5ac2bab6cb40d1cc9bae5f2ab13d
louislooi <louislooi31@gmail.com>
2021-10-10 14:33:23 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,33 @@
+﻿//Initial variables
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+// Setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Setting static files
+app.use(express.static("public"));
+
+// Handle request and response here
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/portfolio", (req, res) => {
+  res.render("portfolio");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+// Start and listen the server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`);
+});
